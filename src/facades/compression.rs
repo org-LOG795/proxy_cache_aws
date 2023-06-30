b8b727378784ee6f277c0839b6c86d5f844ed870
Emeric Duguay <Emericduguay@gmail.com>
2023-06-30 16:49:09 -0400

    init commit file compression working

---
+++ src/facades/compression.rs
@@ -0,0 +1,40 @@
+use std::io::prelude::*;
+use flate2::Compression;
+use flate2::write::GzEncoder;
+
+pub fn compress(bytes: Vec<u8>) -> Vec<u8>
+{
+    let mut e = GzEncoder::new(Vec::new(), Compression::default());
+    e.write_all(&bytes).unwrap();
+
+    match e.finish() {
+        Ok(compressed_bytes) => compressed_bytes,
+        Err(_) => panic!("Failed to compress")
+    }
+}
+
+#[cfg(test)]
+mod compression_test {
+    use super::*;
+    use std::fs;
+    use flate2::read::GzDecoder;
+
+    fn decompress(bytes: Vec<u8>) -> Vec<u8>
+    {
+        let mut d = GzDecoder::new(&bytes[..]);
+        let mut decompressed = Vec::new();
+        d.read_to_end(&mut decompressed).unwrap();
+
+        decompressed
+    }
+
+    #[test]
+    fn compress_valid_file() {
+        let contents = fs::read("T:\\Projects\\proxy_cache_aws\\test_files\\poem.txt").expect("NO FILE FOUND");
+        let compressed = compress(contents.clone());
+        let decompressed = decompress(compressed.clone());
+        assert!(compressed.len() < contents.len());
+        assert_eq!(contents, decompressed);
+        println!("completed");
+    }
+}
