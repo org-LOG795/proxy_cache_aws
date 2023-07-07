a852f6968cd62c4f589dbd38ddb0910489ab5524
carl-kevinc <65026375+carl-kevinc@users.noreply.github.com>
2023-07-07 19:38:26 -0400

    add test of simulated dellay

--- src/facades/compression.rs
+++ src/facades/compression.rs
@@ -6,10 +6,15 @@ use tokio::time::{Duration, sleep};
 
 pub async fn compress(bytes: Vec<u8>, tracer: &impl Tracer) -> Vec<u8>
 {
- 
 
     let _span = tracer.start("compress");
-    sleep(Duration::from_secs(4)).await; // Sleep for 2 seconds
+
+      // Only include the delay code when running tests
+      #[cfg(test)]
+      {
+          sleep(Duration::from_secs(7)).await; // Sleep for 4 seconds
+      }
+
     let mut e = GzEncoder::new(Vec::new(), Compression::default());
     e.write_all(&bytes).unwrap();
 
@@ -45,5 +50,25 @@ mod compression_test {
         assert!(compressed.len() < contents.len());
         assert_eq!(contents, decompressed);
         println!("completed");
-    }    
+    }  
+
+    #[tokio::test]
+    async fn test_compress_7_secs() {
+        let tracer = opentelemetry_jaeger::new_agent_pipeline()
+        .with_service_name("test_delay_7_secs")
+        .install_simple()
+        .expect("Failed to install Jaeger pipeline");
+
+        let bytes = vec![1, 2, 3, 4, 5];
+        
+        let start = std::time::Instant::now();
+        
+        let compressed = compress(bytes.clone(), &tracer).await;
+        
+        let duration = start.elapsed();
+        
+        assert_eq!(compressed, vec![31, 139, 8, 0, 0, 0, 0, 0, 0, 255, 99, 100, 98, 102, 97, 5, 0, 244, 153, 11, 71, 5, 0, 0, 0]);
+        println!("Compression with a delay of 7 seconds took {:?}", duration);
+    } 
+
 }
