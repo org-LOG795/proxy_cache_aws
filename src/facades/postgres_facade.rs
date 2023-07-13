ebfaef257ef861c8def27ee6cd30ed748eb1aa04
Emeric Duguay <Emericduguay@gmail.com>
2023-07-12 22:29:39 -0400

    added unit tests for postgres facade

--- src/facades/postgres_facade.rs
+++ src/facades/postgres_facade.rs
@@ -1,20 +1,34 @@
 use std::env;
-use deadpool_postgres::{Manager, ManagerConfig, Pool, RecyclingMethod, PoolBuilder};
-use tokio_postgres::NoTls;
+use deadpool_postgres::{Manager, ManagerConfig, Pool, RecyclingMethod};
+use tokio_postgres::{Config, NoTls};
 
-pub fn create_pool() -> Pool {
-    let mut pg_config = tokio_postgres::Config::new();
+pub fn create_config_from_env() -> Result<Config, String> {
+    match (env::var("POSTGRES_HOST"),
+           env::var("POSTGRES_USER"),
+           env::var("POSTGRES_PASSWORD"),
+           env::var("POSTGRES_DB")) 
+    {
+        (Ok(host), Ok(user), Ok(password), Ok(db)) => {
+            let mut config = Config::new();
+            config.host(&host);
+            config.user(&user);
+            config.password(&password);
+            config.dbname(&db);
+            Ok(config)
+        },
+        _ => Err("Missing postgres env".to_string())
+    }
+}
 
-    pg_config.host("127.0.0.1");
-    pg_config.user("guest");
-    pg_config.password("guest");
-    pg_config.dbname("test");
-
+pub fn create_pool(configs: Config) -> Result<Pool, String> {
     let mgr_config = ManagerConfig{recycling_method: RecyclingMethod::Fast};
-
-    let mgr = Manager::from_config(pg_config, NoTls, mgr_config);
+    let mgr = Manager::from_config(configs, NoTls, mgr_config);
+    let pool_result = Pool::builder(mgr).max_size(3).build();
+    match pool_result {
+        Ok(pool) => Ok(pool),
+        Err(err) => Err(err.to_string()),
+    }
     
-    Pool::builder(mgr).max_size(3).build().unwrap()
 }
 
 #[cfg(test)]
@@ -22,21 +36,44 @@ mod tests {
     use super::*;
     use std::env;
 
+    fn setup_env() {
+        env::set_var("POSTGRES_HOST", "127.0.0.1");
+        env::set_var("POSTGRES_USER", "guest");
+        env::set_var("POSTGRES_PASSWORD", "guest");
+        env::set_var("POSTGRES_DB", "test");
+    }
+
+    #[test]
+    fn err_if_no_env() {
+        let config = create_config_from_env();
+        assert!(config.is_err());
+    }
+
     #[test]
     fn create_connection_pool() {
-        let pool = create_pool();
+        setup_env();
+        let pg_config = create_config_from_env().unwrap();
+        let pool = create_pool(pg_config);
+        assert!(pool.is_ok());
     }
 
     #[tokio::test]
-    async fn get_client() {
-        let pool = create_pool();
-        let client = pool.get().await.unwrap();
+    async fn create_client() {
+        setup_env();
+        let pg_config = create_config_from_env().unwrap();
+        let pool = create_pool(pg_config);
+        assert!(pool.is_ok());
+        let client = pool.unwrap().get().await;
+        assert!(client.is_ok())
     }
 
     #[tokio::test]
-    async fn test_statement() {
-        let pool = create_pool();
-        let client = pool.get().await.unwrap();
+    async fn select_statement() {
+        setup_env();
+        let pg_config = create_config_from_env().unwrap();
+        let pool = create_pool(pg_config);
+        assert!(pool.is_ok());
+        let client = pool.unwrap().get().await.unwrap();
 
         let statement = client.prepare_cached("SELECT * FROM public.table_test").await.unwrap();
         let rows = client.query(&statement, &[]).await.unwrap();
