51e0b207adc1e2d4d8b580517b192506139d23d3
Emeric Duguay <Emericduguay@gmail.com>
2023-07-14 15:56:25 -0400

    Optimized compression code + modified the unit test

--- src/facades/compression.rs
+++ src/facades/compression.rs
@@ -2,40 +2,48 @@ use std::io::prelude::*;
 use flate2::Compression;
 use flate2::write::GzEncoder;
 
-pub fn compress(bytes: Vec<u8>) -> Vec<u8>
+pub fn gzip_compress(bytes: Vec<u8>) -> Result<Vec<u8>, String>
 {
     let mut e = GzEncoder::new(Vec::new(), Compression::default());
-    e.write_all(&bytes).unwrap();
-
-    match e.finish() {
-        Ok(compressed_bytes) => compressed_bytes,
-        Err(_) => panic!("Failed to compress")
-    }
+    e.write_all(&bytes).and_then( |_| e.finish()).map_err(|op| op.to_string())
 }
 
 #[cfg(test)]
 mod compression_test {
     use super::*;
-    use std::fs;
+    use std::{fs, path::Path};
     use flate2::read::GzDecoder;
 
     //This is only built when we run the unit tests
-    fn decompress(bytes: Vec<u8>) -> Vec<u8>
+    fn gzip_decompress(bytes: Vec<u8>) -> Result<Vec<u8>, String>
     {
         let mut d = GzDecoder::new(&bytes[..]);
         let mut decompressed = Vec::new();
-        d.read_to_end(&mut decompressed).unwrap();
-
-        decompressed
+        d.read_to_end(&mut decompressed).map(|_| decompressed).map_err(|err| err.to_string())
     }
+
+    fn load_test_file() -> Result<Vec<u8>, String> {
+        let path = Path::new("test_files/poem.txt");
+        fs::read(path).map_err(|e| e.to_string())
+    }   
 
     #[test]
     fn compress_valid_file() {
-        let contents = fs::read("T:\\Projects\\proxy_cache_aws\\test_files\\poem.txt").expect("NO FILE FOUND");
-        let compressed = compress(contents.clone());
-        let decompressed = decompress(compressed.clone());
+        let contents = load_test_file();
+        assert!(contents.is_ok());
+        let contents = contents.unwrap();
+
+        let compressed = gzip_compress(contents.clone());
+        assert!(compressed.is_ok());
+        let compressed = compressed.unwrap();
+
+        let decompressed = gzip_decompress(compressed.clone());
+        assert!(decompressed.is_ok());
+        let decompressed = decompressed.unwrap();
+
+        //Assert the amount of bytes for compressed file is less
         assert!(compressed.len() < contents.len());
+        //Assert that the compression is reversible
         assert_eq!(contents, decompressed);
-        println!("completed");
     }
 }
