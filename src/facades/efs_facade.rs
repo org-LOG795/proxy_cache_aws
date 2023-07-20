2f93870115ff0e13c01b9f3fc575ee1f793381c4
MFIslam <mohammed-foridul.islam.1@ens.etsmtl.ca>
2023-07-20 19:10:49 -0400

    Update efs_facade.rs

--- src/facades/efs_facade.rs
+++ src/facades/efs_facade.rs
@@ -1,25 +1,23 @@
 use std::path::Path;
 use tokio::fs;
-use std::io;
-use std::ops::Range<u64>
+use std::io::{Write, Read, Seek, SeekFrom};
+use std::ops::Range<u64>;
+use std::fs::OpenOptions;
 
-pub struct EfsFacade {
-}
+pub struct EfsFacade {}
 
 
 impl<'a> EfsFacade<'a> {
     pub fn new() -> Self {
-
+        EfsFacade {}
     }
 
     pub async fn write(&self, bytes: &[u8], archive_name: String) -> Result<(), Range<u64>> {
 
-        file_path = ""
-
         let mut options = OpenOptions::new();
         options.write(true).create_new(true);
 
-        match options.open(file_path).await {
+        match options.open(archive_name).await {
             Ok(mut file) => {
 
                 let current_offset = file.seek(SeekFrom::Current(0)).await.unwrap();
@@ -32,49 +30,34 @@ impl<'a> EfsFacade<'a> {
                 
                 let new_offset = file.seek(SeekFrom::Current(0)).await.unwrap();
                 let offset_range = current_offset..new_offset;
+
+                Ok(offset_range);
             }
             Err(err) => {
                 println!("Error opening file: {}", err);
             }
         }
-
-        return offset_range
     }
 
-    // not sure about this
-    pub async fn read(&mut self, file_name: &String) -> Result<(), Vec<u8>> {
-        
-        // Need to determine the file naming convention
-        let parts: Vec<&str> = file_name.split('_').collect();
+    pub async fn read(&mut self, offset_range: &Range<u64>, file_name: &str) -> Result<(), Vec<u8>> {
 
-        if let Some(offset_range) = parts.get(1) {
-            
-            let offsets: Vec<&str> = offset_range.split('-').collect();
-
-            if let (Some(start), Some(end)) = (offsets.get(0), offsets.get(1)) {
+        if let Some(start) = offset_range.start {
+            if let Some(end) = offset_range.end {
+                let mut options = OpenOptions::new();
+                let mut file = options.read(true).open(file_name).await?;
                 
-                if let Ok(start_offset) = start.parse::<u64>() {
-                    if let Ok(end_offset) = end.parse::<u64>() {
-                        
-                        let mut options = OpenOptions::new();
-                        let mut file = options.read(true).open(file_name).await.unwrap();
-                            
-                        file.seek(SeekFrom::Start(start_offset)).await.unwrap();
-                        
-                        let bytes_range = (end_offset - start_offset) as usize;
-
-                        
-                        let mut bytes = vec![0u8; bytes_range];
-                        file.read_exact(&mut bytes).await.unwrap();
-
-                        
-                        return bytes;
-                    }
-                }
+                file.seek(SeekFrom::Start(start)).await?;
+                
+                let bytes_range = (end - start) as usize;
+                let mut bytes = vec![0u8; bytes_range];
+                file.read_exact(&mut bytes).await?;
+                
+                Ok(bytes);
+            } else {
+                Err("Error reading the end offset".into())
             }
+        } else {
+            Err("Error reading the start of the offset".into())
         }
-
-        Err("Error occured whiel reading bytes".into())
-        
     }
 }
