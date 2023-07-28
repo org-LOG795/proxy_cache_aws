d0354a39d8198b42994c27bf47f16deb5530a535
obidobo <gsheremetov@gmail.com>
2023-07-27 23:48:32 -0400

    Combine files in directory for a multipart upload

--- src/facades/archivist.rs
+++ src/facades/archivist.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
-
+use tokio::fs::{self, File};
 use tracing_subscriber::filter::Directive;
+use tokio::io::AsyncWriteExt;
 
 use super::{efs_facade::EfsFacade, s3::S3Facade};
 
@@ -10,6 +11,7 @@ pub struct Archivist {
 }
 
 impl Archivist {
+   
     pub fn new(efs_facade: EfsFacade, s3_facade: S3Facade) -> Self {
         Archivist {
             efs_facade,
@@ -18,27 +20,85 @@ impl Archivist {
     }
 
     //Read from EFS and write to an S3 bucket
-    pub async fn archive_to_s3(&self, file_name: &str, bucket_name: &str, part_size: usize) -> Result<(), Box<dyn Error>> {
+    pub async fn archive_to_s3(
+        &self,
+        file_name: &str,
+        bucket_name: &str,
+        part_size: usize,
+    ) -> Result<(), Box<dyn Error>> {
         let path = "temp";
-        let directories_list = self.efs_facade.get_directories_list(path);
-        let file_bytes = self.efs_facade.read(file_name).await?;
-        self.s3_facade.upload_file_multipart(bucket_name, file_name, file_name, part_size).await?;
+
+        //TODO : name combined file accordigly
+        let combined_file_name = "combined_file.txt"; 
+        self.get_files_from_efs().await?; // Combine the files and write to combined_file.txt
+
+        // Upload the combined file to S3 using multipart
+        self.s3_facade
+            .upload_file_multipart(bucket_name, &combined_file_name, file_name, part_size)
+            .await?;
+
+        // Optional: Delete the combined file after uploading to S3
+        let combined_file_path = format!("{}/{}", path, combined_file_name);
+        fs::remove_file(&combined_file_path).await?;
+
         Ok(())
     }
 
-    async fn get_files_from_efs(&self){
+    async fn get_files_from_efs(&self) -> Result<(), Box<dyn Error>> {
         let path = "temp";
-        let directories_list = self.efs_facade.get_directories_list(path).await;
+        let directories_list = self.efs_facade.get_directories_list(path).await?;
 
-        for directories in &directories_list{
+        let mut combined_bytes = Vec::new();
 
-            // if manifest file: add into a json
+        for directory_name in &directories_list {
 
-            // if file: send to S3 bucket
+            let manifest_str = ".manifest".to_string();
+
+            // Skip manifest files and offset
+            if !directory_name.ends_with(manifest_str.as_str()) && *directory_name != "offset" {
+                let file_path = format!("{}/{}", path, directory_name);
+                let file_bytes = self.efs_facade.read(&file_path).await?;
+                combined_bytes.extend_from_slice(&file_bytes);
+            }
+        }
+
+        // Write the combined bytes to temp file
+        let combined_file_name = "combined_file.txt";
+        let combined_file_path = format!("{}/{}", path, combined_file_name);
+        let mut combined_file = File::create(&combined_file_path).await?;
+        combined_file.write_all(&combined_bytes).await?; 
+
+        Ok(())
+
+    }
 
 
+    async fn get_file_size(&self, file_path: &str) -> u64 {
+        if let Ok(metadata) = fs::metadata(file_path).await {
+            metadata.len()
+        } else {
+            0
+        }
+    }
+
+    fn calculate_part_size(&self, file_size: u64) -> usize {
+        // Adjust the part size calculation according to the file size
+        if file_size > 5_000_000_000_000 {
+            // If file size is larger than 5TB
+            100_000_000 // Use a part size of 100MB
+        } else if file_size > 100_000_000 {
+            // If file size is larger than 100MB
+            10_000_000 // Use a part size of 10MB
+        } else {
+            // If file size is smaller than 100MB
+            8_000_000 // Use the default part size of 8MB
         }
     }
 
 }
 
+
+#[cfg(test)]
+mod tests {
+
+}
