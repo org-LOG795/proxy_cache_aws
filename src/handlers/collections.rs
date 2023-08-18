1bb9f00827556fbcd33c57af14fbd7accbdb19ed
MFIslam <mohammed-foridul.islam.1@ens.etsmtl.ca>
2023-08-18 00:36:41 -0400

    Update collections.rs

--- src/handlers/collections.rs
+++ src/handlers/collections.rs
@@ -4,81 +4,50 @@ use std::time::Instant;
 use crate::facades::efs_facade::Metadata;
 
 use super::super::facades;
-use axum::extract::{State, Path, Query};
-use axum::{response::IntoResponse, http::{StatusCode, header::{self, HeaderMap, HeaderName}}};
+use axum::extract::{Path, Query, State};
+use axum::{
+    http::{
+        header::{self, HeaderMap, HeaderName},
+        StatusCode,
+    },
+    response::IntoResponse,
+};
 use deadpool_postgres::{Object, Pool};
 use facades::compression::gzip_compress;
-use facades::efs_facade::{get_collection_byte_range as read_efs, append_bytes_collection as write_efs, write_metadata};
-use facades::s3::{read_file as read_s3, init_client as init_s3_client};
+use facades::efs_facade::{
+    append_bytes_collection as write_efs, get_collection_byte_range as read_efs, write_metadata,
+};
+use facades::s3::{init_client as init_s3_client, read_file as read_s3};
 use hyper::body::to_bytes;
-use hyper::{Request, Body, Method};
+use hyper::{Body, Method, Request};
 
 pub async fn collection_handler(
     Path(collection): Path<String>,
-    request: Request<Body>
+    request: Request<Body>,
 ) -> impl IntoResponse {
-        match *request.method() {
-            Method::GET => {
-                let params = extract_query_params(&request.uri().to_string());
-                match (params.get("start").map(|s| s.parse::<u64>()), params.get("end").map(|e| e.parse::<u64>())) {
-                    (Some(Ok(start)), Some(Ok(end))) => {
-                        match get_handler(collection, start, end).await {
-                            Ok(Some(bytes)) => {
-                                let mut headers = HeaderMap::new();
-                                headers.insert(header::CONTENT_TYPE, "application/octet-stream".parse().unwrap());
-                                headers.insert(header::CONTENT_ENCODING, "gzip".parse().unwrap());
-                                (StatusCode::OK, headers, bytes).into_response()
-                            },
-                            Ok(None) => (StatusCode::NOT_FOUND, "unable to find supplied byte range".to_string()).into_response(),
-                            Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err).into_response(),
-                        }
-                    },
-                    _ => (StatusCode::BAD_REQUEST, "unable to parse start & end params".to_string()).into_response()
-                }
-            },
-            Method::POST => {
-                let content_type = request.headers().get("Content-Type").unwrap().to_str().unwrap().to_string();
-                let host = request.headers().get("Host").unwrap().to_str().unwrap().to_string();
-                let bytes = to_bytes(request.into_body()).await.unwrap().to_vec();
-                match post_handler(collection, bytes, content_type, host).await{
-                    Ok(file_path) => (StatusCode::OK, file_path).into_response(),
-                    Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err).into_response()
-                }
-            },
-            // Method::PATCH => {
-            //     let params = extract_query_params(&request.uri().to_string());
-            //     let bytes = to_bytes(request.into_body()).await.unwrap().to_vec();
-            //     match (params.get("start").map(|s| s.parse::<i64>()), params.get("end").map(|e| e.parse::<i64>())) {
-            //         (Some(Ok(start)), Some(Ok(end))) => {
-            //             match patch_handler(collection, bytes, start, end).await {
-            //                 Ok(file_path) => (StatusCode::OK, file_path).into_response(),
-            //                 Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err).into_response()
-            //             }
-            //         },
-            //         _ => (StatusCode::BAD_REQUEST, "unable to parse start & end params".to_string()).into_response()
-            //     }
-            // },
-            _ => StatusCode::NOT_FOUND.into_response()
-        }
-        Method::POST => {
-            let client = pool_manager.get().await.unwrap();
-            let bytes = to_bytes(request.into_body()).await.unwrap().to_vec();
-
-            match post_handler(collection, bytes, client).await {
-                Ok(file_path) => (StatusCode::OK, file_path).into_response(),
-                Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err).into_response(),
-            }
-        }
-        Method::PATCH => {
+    match *request.method() {
+        Method::GET => {
             let params = extract_query_params(&request.uri().to_string());
-            let bytes = to_bytes(request.into_body()).await.unwrap().to_vec();
             match (
-                params.get("start").map(|s| s.parse::<i64>()),
-                params.get("end").map(|e| e.parse::<i64>()),
+                params.get("start").map(|s| s.parse::<u64>()),
+                params.get("end").map(|e| e.parse::<u64>()),
             ) {
                 (Some(Ok(start)), Some(Ok(end))) => {
-                    match patch_handler(collection, bytes, start, end).await {
-                        Ok(file_path) => (StatusCode::OK, file_path).into_response(),
+                    match get_handler(collection, start, end).await {
+                        Ok(Some(bytes)) => {
+                            let mut headers = HeaderMap::new();
+                            headers.insert(
+                                header::CONTENT_TYPE,
+                                "application/octet-stream".parse().unwrap(),
+                            );
+                            headers.insert(header::CONTENT_ENCODING, "gzip".parse().unwrap());
+                            (StatusCode::OK, headers, bytes).into_response()
+                        }
+                        Ok(None) => (
+                            StatusCode::NOT_FOUND,
+                            "unable to find supplied byte range".to_string(),
+                        )
+                            .into_response(),
                         Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err).into_response(),
                     }
                 }
@@ -89,6 +58,40 @@ pub async fn collection_handler(
                     .into_response(),
             }
         }
+        Method::POST => {
+            let content_type = request
+                .headers()
+                .get("Content-Type")
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string();
+            let host = request
+                .headers()
+                .get("Host")
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string();
+            let bytes = to_bytes(request.into_body()).await.unwrap().to_vec();
+            match post_handler(collection, bytes, content_type, host).await {
+                Ok(file_path) => (StatusCode::OK, file_path).into_response(),
+                Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err).into_response(),
+            }
+        }
+        // Method::PATCH => {
+        //     let params = extract_query_params(&request.uri().to_string());
+        //     let bytes = to_bytes(request.into_body()).await.unwrap().to_vec();
+        //     match (params.get("start").map(|s| s.parse::<i64>()), params.get("end").map(|e| e.parse::<i64>())) {
+        //         (Some(Ok(start)), Some(Ok(end))) => {
+        //             match patch_handler(collection, bytes, start, end).await {
+        //                 Ok(file_path) => (StatusCode::OK, file_path).into_response(),
+        //                 Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err).into_response()
+        //             }
+        //         },
+        //         _ => (StatusCode::BAD_REQUEST, "unable to parse start & end params".to_string()).into_response()
+        //     }
+        // },
         _ => StatusCode::NOT_FOUND.into_response(),
     }
 }
@@ -99,7 +102,7 @@ pub async fn collection_handler(
 3. Check S3 (return if found)
 4. If nothing found... cry :(
 */
-async fn get_handler(collection: String, start: u64, end: u64) -> Result<Option<Vec<u8>>, String>{
+async fn get_handler(collection: String, start: u64, end: u64) -> Result<Option<Vec<u8>>, String> {
     let mut res: Option<Vec<u8>> = read_efs(collection, start, end).await?;
 
     if res.is_none() {
@@ -118,7 +121,12 @@ async fn get_handler(collection: String, start: u64, end: u64) -> Result<Option<
 4. Send to EFS
 5. Return file name
 */
-async fn post_handler(collection: String, bytes: Vec<u8>, content_type: String, host: String) -> Result<String, String> {
+async fn post_handler(
+    collection: String,
+    bytes: Vec<u8>,
+    content_type: String,
+    host: String,
+) -> Result<String, String> {
     //Start the timer
     // let compress_start = Instant::now();
     match gzip_compress(bytes) {
@@ -126,12 +134,26 @@ async fn post_handler(collection: String, bytes: Vec<u8>, content_type: String, 
             // println!("COMPRESS => {}ms", compress_start.elapsed().as_millis().to_string());
 
             // let write_efs_start = Instant::now();
-            let write_res = write_efs(collection.clone(), compressed).await.map_err(|e| e.to_string())?;
-            let formatted_path = format!("{file}?start={start}&end={end}",file = write_res.0, start = write_res.1, end = write_res.2).to_string();
+            let write_res = write_efs(collection.clone(), compressed)
+                .await
+                .map_err(|e| e.to_string())?;
+            let formatted_path = format!(
+                "{file}?start={start}&end={end}",
+                file = write_res.0,
+                start = write_res.1,
+                end = write_res.2
+            )
+            .to_string();
             // println!("EFS => {}ms", write_efs_start.elapsed().as_millis().to_string());
-            
+
             // let meta_start = Instant::now();
-            let meta = Metadata::new(content_type, "gzip".to_string(), host, write_res.1, write_res.2);
+            let meta = Metadata::new(
+                content_type,
+                "gzip".to_string(),
+                host,
+                write_res.1,
+                write_res.2,
+            );
             write_metadata(collection, meta).await?;
             // println!("META => {}ms", meta_start.elapsed().as_millis().to_string());
 
@@ -172,8 +194,10 @@ mod tests {
     use std::{fs, path::Path};
 
     use super::*;
-    use facades::compression::{gzip_decompress as decompress, gzip_compress as compress};
+    use chrono::{Datelike, Utc};
+    use facades::compression::{gzip_compress as compress, gzip_decompress as decompress};
     use facades::efs_facade::get_file_path;
+    use tokio_postgres::Config;
 
     fn load_test_files() -> Vec<Vec<u8>> {
         let mut files: Vec<Vec<u8>> = Vec::new();
@@ -235,5 +259,4 @@ mod tests {
     //         assert_eq!(decompress, bytes.clone())
     //     }
     // }
-
 }
