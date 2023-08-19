896a41dcf8d0ef098a81c67dc877c17156cd05a7
MFIslam <mohammed-foridul.islam.1@ens.etsmtl.ca>
2023-08-19 13:55:57 -0400

    Fixed manifest to json problem

--- src/facades/archivist.rs
+++ src/facades/archivist.rs
@@ -9,45 +9,6 @@ use tracing_subscriber::filter::Directive;
 use super::efs_facade::{self, Metadata};
 use super::s3::{self};
 
-async fn read_file(file_path: &String) -> Result<Vec<u8>, Box<dyn Error>> {
-    let mut options = OpenOptions::new();
-    options.read(true);
-    let mut buffer = Vec::new();
-
-    match options.open(file_path).await {
-        Ok(mut file) => {
-            file.read_to_end(&mut buffer).await?;
-            println!("Bytes were read from file: {}", file_path);
-        }
-
-        Err(e) => {
-            println!("Error reading file: {}, {}", file_path, e);
-            return Err(Box::new(e));
-        }
-    }
-
-    Ok(buffer)
-}
-
-async fn write_file(file_path: &String, bytes: &[u8]) -> Result<(), Box<dyn Error>> {
-    let mut options = OpenOptions::new();
-    let output_options = options.write(true).create_new(true);
-
-    match output_options.open(file_path).await {
-        Ok(mut file) => {
-            file.write_all(bytes).await?;
-            println!("Bytes were written into file: {}", file_path);
-        }
-
-        Err(e) => {
-            println!("Error writting into file: {}, {}", file_path, e);
-            return Err(Box::new(e));
-        }
-    }
-
-    Ok(())
-}
-
 //Read from EFS and write to an S3 bucket
 pub async fn archive_to_s3(
     master_directory_path: &str,
@@ -55,6 +16,7 @@ pub async fn archive_to_s3(
     part_size: usize,
 ) -> Result<(), String> {
     let directories_list = efs_facade::get_directories_list(master_directory_path).await;
+    let s3_client = s3::init_client();
 
     let directories_list = match directories_list {
         Ok(directories_list) => directories_list,
@@ -66,17 +28,21 @@ pub async fn archive_to_s3(
     for directory in directories_list {
         let directory_path = format!("{}/{}", master_directory_path, directory);
         let file_size = get_file_size(&directory_path.clone()).await;
-        let part_size = calculate_part_size(file_size);
-
-        let s3_client = s3::init_client();
+        let part_size = calculate_part_size(file_size).await;
 
         let output_file_name = format!("{}", directory);
         let output_file_path = format!("{}/{}", directory_path, directory);
 
-        if let Ok(_metadata) = fs::metadata(output_file_path).await {
+        let file_size = get_file_size(&output_file_path.clone()).await;
+        let part_size = calculate_part_size(file_size).await;
+
+        println!("File Size: {}", file_size);
+        println!("Part Size: {}", part_size);
+
+        if let Ok(metadata) = fs::metadata(output_file_path.clone()).await {
             match s3::upload_file_multipart(
                 bucket_name,
-                &directory_path.clone(),
+                &output_file_path.clone(),
                 &output_file_name,
                 part_size,
                 s3_client.clone(),
@@ -93,13 +59,20 @@ pub async fn archive_to_s3(
         let manifest_file_name = format!("{}.manifest", directory);
         let manifest_file_path = format!("{}/{}", directory_path, manifest_file_name);
 
-        if let Ok(_metadata) = fs::metadata(manifest_file_path.clone()).await {
-            let manifest_bytes = read_file(&manifest_file_path).await;
+        if let Ok(metadata) = fs::metadata(manifest_file_path.clone()).await {
+            let manifest_bytes = read_from_manifest(&manifest_file_path).await;
 
             let json_manifest_name = format!("{}-manifest.json", directory);
             let json_manifest_path = format!("{}/{}", directory_path, json_manifest_name);
 
-            let _ = write_file(&json_manifest_path, &manifest_bytes.unwrap()).await;
+            let serialized_manifest = serde_json::to_string_pretty(&manifest_bytes.unwrap());
+
+            _ = write_file(
+                &json_manifest_path,
+                &serialized_manifest.unwrap().as_bytes(),
+            )
+            .await;
+
             match s3::upload_file_multipart(
                 bucket_name,
                 &directory_path.clone(),
@@ -117,7 +90,7 @@ pub async fn archive_to_s3(
         }
 
         let directory_path_for_delete = format!("{}/{}", master_directory_path, directory);
-        let _ = fs::remove_dir_all(directory_path_for_delete).await;
+        _ = fs::remove_dir_all(directory_path_for_delete).await;
         println!("File path deleted: {}", directory);
     }
 
@@ -139,6 +112,25 @@ async fn read_from_manifest(file_path: &str) -> Result<Vec<Metadata>, Box<dyn st
     Ok(segments)
 }
 
+async fn write_file(file_path: &String, bytes: &[u8]) -> Result<(), Box<dyn Error>> {
+    let mut options = OpenOptions::new();
+    let output_options = options.write(true).create_new(true);
+
+    match output_options.open(file_path).await {
+        Ok(mut file) => {
+            file.write_all(bytes).await?;
+            println!("Bytes were written into file: {}", file_path);
+        }
+
+        Err(e) => {
+            println!("Error writting into file: {}, {}", file_path, e);
+            return Err(Box::new(e));
+        }
+    }
+
+    Ok(())
+}
+
 async fn get_file_size(file_path: &str) -> u64 {
     if let Ok(metadata) = fs::metadata(file_path).await {
         metadata.len()
@@ -147,7 +139,7 @@ async fn get_file_size(file_path: &str) -> u64 {
     }
 }
 
-fn calculate_part_size(file_size: u64) -> usize {
+async fn calculate_part_size(file_size: u64) -> usize {
     if file_size > 5_000_000_000_000 {
         100_000_000 //100MB
     } else if file_size > 100_000_000 {
@@ -167,7 +159,7 @@ mod archivist_test {
         let directory_name = "test-directory";
 
         //let archivist = archive_to_s3(directory_name, "bucket", 64).await;
-        let archivist = archive_to_s3(directory_name, "rusty-bucket-2834", 1).await;
+        let archivist = archive_to_s3(directory_name, "rusty-bucket-2834", 8000000).await;
         assert!(archivist.is_ok());
         // let deleted = efs_facade::delete(directory_name).await;
         // assert!(deleted.is_ok());
